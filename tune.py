4645a5d2082f401b0ebcd6b5bfea29dbde989cd1
Anton Kulaga <antonkulaga@gmail.com>
2020-08-09 11:41:47 +0300

    tuning update

--- tune.py
+++ tune.py
@@ -17,18 +17,24 @@ def get_local_path():
     return local
 
 @click.command()
-def tune():
-    print("starting hyperparameters optimization script")
-    number_of_folds = 5
-    #time_budget_seconds = 200
-    n_trials = 5
-    threads = 1
+@click.option('--name', default="general_tuner", help='study name')
+@click.option('--trials', default=10, help='Number of trials in hyper optimization')
+@click.option('--folds', default=5, help='Number of folds in cross-validation')
+@click.option('--hold_outs', default=1, help='Number of hold outs in cross-validation')
+@click.option('--threads', default=1, help="number of threads (1 by default). If you put -1 it will try to utilize all cores, however it can be dangerous memorywise")
+@click.option('--species_in_validation', default=3, help="species_in_validation")
+@click.option('--not_validated_species', default="Homo_sapiens", help="not_validated_species")
+def tune(name, trials, folds, hold_outs, threads, species_in_validation, not_validated_species):
+    print(f"starting hyperparameters optimization script with {trials} trials, {folds} folds and {hold_outs} hold outs!")
     local = get_local_path()
+
+    not_validated_species = [not_validated_species] if type(not_validated_species) is str else not_validated_species
 
     from yspecies.dataset import ExpressionDataset
     from yspecies.partition import DataPartitioner, FeatureSelection, DataExtractor
     from yspecies.tuning import GeneralTuner, TuningResults
     from yspecies.workflow import Locations
+    from yspecies.models import Metrics
 
     locations: Locations = Locations("./") if Path("./data").exists() else Locations("../")
 
@@ -43,7 +49,7 @@ def tune():
 
     ext = Pipeline([
         ('extractor', DataExtractor(selection)), # to extract the data required for ML from the dataset
-        ("partitioner", DataPartitioner(nfolds = number_of_folds, nhold_out = 1, species_in_validation=2, not_validated_species = ["Homo_sapiens"]))
+        ("partitioner", DataPartitioner(nfolds = folds, nhold_out = 1, species_in_validation=species_in_validation, not_validated_species = not_validated_species))
     ])
     parts = ext.fit_transform(data)
     assert (len(parts.cv_merged_index) + len(parts.hold_out_merged_index)) == data.samples.shape[0], "cv and hold out should be same as samples number"
@@ -56,7 +62,7 @@ def tune():
         #engine_kwargs={'check_same_thread': False}
     )
     study = optuna.create_study(storage, study_name="general_tuner", direction='minimize', load_if_exists=True)
-    tuner = GeneralTuner(n_trials = n_trials, n_jobs = threads, study = study)
+    tuner = GeneralTuner(n_trials = trials, n_jobs = threads, study = study)
     best_parameters = tuner.fit(parts)
     print("======BEST=PARAMETERS===============")
     print(best_parameters)
@@ -67,8 +73,8 @@ def tune():
     import json
     with open(locations.output.optimization / 'parameters.json', 'w') as fp:
         json.dump(best_parameters, fp)
-    with open(locations.output.optimization / 'results.json', 'w') as fp:
-        json.dump(results, fp)
+    if results.train_metrics is not None and results.validation_metrics is not None:
+        Metrics.combine([results.train_metrics, results.validation_metrics]).to_csv(locations.output.optimization / 'metrics.tsv', sep="\t")
 
 if __name__ == "__main__":
     tune()
