b1bc887f23eec045da6fe37ee5a0991ec447c638
Anton Kulaga <antonkulaga@gmail.com>
2020-08-19 09:17:41 +0300

    changed selected to be more representative, modified notebooks

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 setup(name='yspecies',
-      version='0.2.1',
-      py_modules=['yspecies', 'dataset', 'workflow', 'utils', 'partition', "selection", "results", 'tuning', 'model'],
+      version='0.2.2',
+      py_modules=['yspecies', 'dataset', 'workflow', 'utils', 'partition', "selection", "results", 'tuning', 'model', 'pipelines'],
       packages=find_packages(),
       description='yspecies')
